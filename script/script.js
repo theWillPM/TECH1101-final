41460a8c23e34794247fbba9a7076f9a748c4726
Willian P. Munhoz <96987268+theWillPM@users.noreply.github.com>
2022-12-11 11:19:10 -0700

    Online Estimator is now working
    Need to refactor the code and adjust some styling

--- script/script.js
+++ script/script.js
@@ -1,5 +1,28 @@
 let hamburguerMenuIcon = document.getElementById("hamburguer-menu");
 let hamburguerMenuContent = document.getElementById("menu");
+let itemQuantitiesArray = document.getElementsByClassName("user-input-quantity");
+let priceArray = document.querySelectorAll('.subtotal');
+let totalPrice = 0;
+
+//Thank you, @Erik from StackOverflow for this solution: (https://stackoverflow.com/a/37335153) (Research by Willian)
+Array.prototype.forEach.call(itemQuantitiesArray, update);
+
+function update(val, i){
+    val.addEventListener('input', function(){
+        var x = val.value;
+        let subtotal = (x*document.getElementsByClassName('price-description')[i].getAttribute("data-amount")).toFixed(2); 
+    priceArray[i].innerHTML = subtotal;
+        updateTotal();
+    });
+  };
+
+function updateTotal () {  
+    totalPrice = 0;
+  priceArray.forEach(element => {
+    totalPrice += parseFloat(element.innerHTML);
+    document.getElementById('total').innerHTML =  "$" + totalPrice;
+});   
+}
 
 function openHamburguerMenu() {
     hamburguerMenuContent.style.width = "250px";
